194666f44991618c28bd9c4300753e2e89f97448
AlexRixten <rixten.09@gmail.com>
2023-01-22 19:24:34 +0300

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import { authSelector } from "./store/selectors/authSelector";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { ERoutes } from "./enums/routes.enum";
 import { HomePage } from "./pages/Home/HomePage";
+import { AboutUsPage } from "./pages/AboutUs/AboutUsPage";
+import { BlogPage } from "./pages/Blog/BlogPage";
+import { ContactsPage } from "./pages/Contacts/ContactsPage";
+import { CoursePage } from "./pages/Course/CoursePage";
+import { CoursesPage } from "./pages/Courses/CoursesPage";
+import { EventsPage } from "./pages/Events/EventsPage";
+import { EventPage } from "./pages/Event/EventPage";
+import { PostPage } from "./pages/Post/PostPage";
+import { NotFoundPage } from "./pages/NotFound/NotFoundPage";
 // import { MainLayout } from "./layouts/mainLayout/MainLayout";
 
 function App() {
@@ -20,6 +29,15 @@ function App() {
 		// <MainLayout>
 			<Routes>
 				<Route path={ERoutes.Home} element={<HomePage />} />
+				<Route path={ERoutes.AboutUs} element={<AboutUsPage />} />
+				<Route path={ERoutes.Blogs} element={<BlogPage />} />
+				<Route path={ERoutes.Contacts} element={<ContactsPage />} />
+				<Route path={ERoutes.Course} element={<CoursePage />} />
+				<Route path={ERoutes.Courses} element={<CoursesPage />} />
+				<Route path={ERoutes.Events} element={<EventsPage />} />
+				<Route path={ERoutes.Event} element={<EventPage />} />
+				<Route path={ERoutes.Post} element={<PostPage />} />
+				<Route path={ERoutes.NotFound} element={<NotFoundPage />} />
 			</Routes>
 		// </MainLayout>
 	);
--- src/enums/routes.enum.ts
+++ src/enums/routes.enum.ts
@@ -1,4 +1,13 @@
 export enum ERoutes {
 	Home = '/',
-	SignIn = '/signin',
+	SignIn = '/sign_in',
+	Contacts = '/contacts',
+	Blogs = '/blogs',
+	Courses = '/courses',
+	Events = '/events',
+	Course = '/course/:courseId',
+	Event = '/event/:eventId',
+	AboutUs = '/about_us',
+	Post = '/post/:postId',
+	NotFound = '/not_found',
 }
