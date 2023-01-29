ae18e7e9e63b2ba93c5444195e9c1db5b5ca5adc
AlexRixten <rixten.09@gmail.com>
2023-01-29 10:19:03 +0300

    create blogs module

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -3,13 +3,16 @@ import { Icon } from "../Icon";
 
 interface IProps{
 	name: string;
+	class?: string;
+	title: string;
 }
 
 export const Badge = (props:IProps) => {
+	const className = props.class ? styles[props.class] : null
 	return(
-		<div className={styles.badge}>
+		<div className={`${styles.badge} ${className}`}>
 			<Icon size={16} color='#424551' name={props.name} />
-
+			<h3>{props.title}</h3>
 		</div>
 	)
 }
--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -1,10 +1,39 @@
 import { IBlog } from "../../interfaces/blogs";
+import { Badge } from "../Badge/Badge";
+
+import styles from "./BlogCard.module.scss";
+import { Icon } from "../Icon";
+import { Link } from "react-router-dom";
 
 export const BlogCard = (props: IBlog) => {
-	const {title} = props;
+	const { id, type, nameType, title, description, date, duration, linkTitle, tag, imgUrl } = props;
 	return (
-		<>
-			{title}
-		</>
-	)
-}
+		<div className={styles.blogCard}>
+			<Badge class="absolute" name={type} title={nameType} />
+			<img src={imgUrl} alt={imgUrl} />
+			<div className={styles.meta}>
+				<span>{tag}</span>
+				<div className={styles.divider}></div>
+				<div className={styles.date}>
+					<Icon size={14} color="#787A80" name="calendar" />
+					{date}
+				</div>
+				<div className={styles.divider}></div>
+				<div className={styles.duration}>
+					<Icon size={14} color="#787A80" name="watch" />
+					{duration} min
+				</div>
+			</div>
+			<p className={styles.title}>
+				{title}
+			</p>
+			<p className={styles.description}>
+				{description}
+			</p>
+			<Link className={styles.link} to={`post/${id}`}>
+				{linkTitle}
+				<Icon size={14} color="#FF3F3A" name="arrowNext" />
+			</Link>
+		</div>
+	);
+};
