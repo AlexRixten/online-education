28668796cb2b9ffb2fc41cdd5c865198b6e2562b
AlexRixten <rixten.09@gmail.com>
2023-02-27 09:03:02 +0300

    change CoursesPage

--- src/pages/Courses/CoursesPage.tsx
+++ src/pages/Courses/CoursesPage.tsx
@@ -1,5 +1,22 @@
+import styles from "./CoursesPage.module.scss";
+import { Title } from "../../components/Title/Title";
+import { Subtitle } from "../../components/Subtitle/Subtitle";
+import React from "react";
+import { CustomInputSearch } from "../../components/UI/inputs/searchInput/CustomInputSearch";
+import FilterTags from "../../components/Tags/FilterTags/FilterTags";
+
 export const CoursesPage = () => {
 	return (
-		<h1>CoursesPage</h1>
+		<section className={styles.coursePage}>
+			<div className="container">
+				<Title text="Enjoy your studying!" center={true} />
+				<Subtitle text="Our online courses" center={true} />
+				<div className="top">
+					<div className="filters"><FilterTags /></div>
+					<CustomInputSearch placeholder="Search course..." value="" onChange={() => {
+					}} />
+				</div>
+			</div>
+		</section>
 	);
 };
